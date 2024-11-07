9c44c29cd6a289e19a52901ea8fb3c015016df98
bipin <vsinghvipin333@gmail.com>
2024-11-07 22:10:03 +0530

    07-11-2024

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
 import { LayoutLoader } from "./components/layout/Loaders";
-
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
 const NotFound = lazy(() => import("./pages/NotFound"));
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+const ChatManagement = lazy(() => import("./pages/admin/ChatManagement"));
+const MessageManagement = lazy(() => import("./pages/admin/MessageManagement"));
+const UserManagement = lazy(() => import("./pages/admin/UserManagement"));
 
 const App = () => {
-  let user = true;
+  let user = false;
   return (
     <BrowserRouter>
       <Suspense fallback={<LayoutLoader />}>
@@ -28,6 +32,11 @@ const App = () => {
               </ProtectRoute>
             }
           />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/users" element={<UserManagement />} />
+          <Route path="/admin/messages" element={<MessageManagement />} />
+          <Route path="/admin/chats" element={<ChatManagement />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
---
+++ client/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import AdminLayout from "../../components/layout/AdminLayout";
+import {
+  AdminPanelSettings as AdminPanelSettingsIcon,
+  Group as GroupIcon,
+  Message as MessageIcon,
+  Notifications as NotificationsIcon,
+  Person as PersonIcon,
+} from "@mui/icons-material";
+import {
+  Box,
+  Container,
+  Paper,
+  Skeleton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import moment from "moment";
+import {
+  CurveButton,
+  SearchField,
+} from "../../components/styles/StyledComponents";
+
+const Dashboard = () => {
+  const AppBar = (
+    <Paper
+      elevation={3}
+      sx={{ padding: "2rem", margin: "2rem 0", borderRadius: "1rem" }}
+    >
+      <Stack direction={"row"} alignItems={"center"} spacing={"1rem"}>
+        <AdminPanelSettingsIcon sx={{ fontSize: "3rem" }} />
+        <SearchField placeholder="Search..." />
+        <CurveButton>Search</CurveButton>
+        <Box flexGrow={1} />
+        <Typography
+          display={{
+            xs: "none",
+            lg: "block",
+          }}
+          color={"rgba(0,0,0,0.7)"}
+          textAlign={"center"}
+        >
+          {moment().format("dddd, D MMMM YYYY")}
+        </Typography>
+        <NotificationsIcon />
+      </Stack>
+    </Paper>
+  );
+  return (
+    <AdminLayout>
+      <Container component={"main"}>{AppBar}</Container>
+    </AdminLayout>
+  );
+};
+
+export default Dashboard;
